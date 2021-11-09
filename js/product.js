6acd22a7e41f57287f725b7ee71c68d97ecbe965
Goyush Mammadov <gmammadov@e2r6p2.unit.ua>
2021-11-09 15:57:55 +0200

    fixed something

--- js/product.js
+++ js/product.js
@@ -1,11 +1,14 @@
+"use strict";
+
+
 const productImages = document.querySelectorAll(".product-img  img");
-const productImageSlide = document.querySelectorAll(".image-slider");
+const productImageSlide = document.querySelector('.image-slider');
 let activeImageSlide = 0;
 productImages.forEach((item, i) =>{
     item.addEventListener("click",()=>{
-        productImages[activeImageSlide].classList.remove("active");
+        productImages[activeImageSlide].classList.remove('active');
         item.classList.add("active");
-        productImages.style.backgroundImage = `url("${item.src}")`;
+        productImageSlide.style.backgroundImage = `url('${item.src}')`;
         activeImageSlide = i;
     })
 })
@@ -13,6 +16,7 @@ productImages.forEach((item, i) =>{
 // toggle button
 const sizeBtns = document.querySelectorAll('.size-radio-btn');
 let  checkedBtn = 0;
+
 sizeBtns.forEach((item, i) =>{
     item.addEventListener('click', () =>{
         sizeBtns[checkedBtn].classList.remove('check');
