4307c21c90d5aca8909cfe928e749aed8afd0206
asamson <Freefire25>
2021-11-14 16:58:25 +0200

    fix bug, add new content

--- js/lang.js
+++ js/lang.js
@@ -34,6 +34,128 @@ const langArr = {
         "en": "Contacts",
         "ua": "Контакти",
     },
+    "aboutSectionAbout":{
+        "ru": "Про нас",
+        "en": "About",
+        "ua": "Про нас",
+    },
+    "aboutSectionP1":{
+        "ru": "О компании",
+        "en": "About company",
+        "ua": "Про компанію",
+    },
+    "aboutSectionText": {
+        "ru": "«uStore» — это хорошо заметный и узнаваемый ритейл на рынке Украины, включающий сеть из более 66 магазинов и интернет-магазин uStore.ua.\n" +
+            "Мы работаем в уникальном для Украины формате инновационных развлекательных центров в ярком современном дизайне, площадью до 2000 м².\n" +
+            "Это открытый доступ к товарам, интерактивные зоны виртуальной реальности, электротранспорт и устройства умного дома, дружелюбный и грамотный персонал. Место, где вам всегда рады.\n" +
+            "</br>" + "</br>" +
+            "Целевая аудитория uStoreа – активные, продвинутые пользователи девайсов и гаджетов: от наушников до ноутбуков, от квадрокоптеров до электробайков.\n" +
+            "Мы целенаправленно занимаемся популяризацией электротранспорта в Украине, первые выделили персональный в отдельную категорию и познакомили украинцев с этим способом передвижения.\n" +
+            "В 2016 году при поддержке uStoreа был проведен первый чемпионат Украины по моноколесу.\n" +
+            "</br>" + "</br>" +
+            "Онлайн магазин uStore.ua уверенно держится среди лидеров интернет-торговли в Украине.\n" +
+            "Центральный офис находится в Одессе (доступна виртуальная экскурсия).\n" +
+            "</br>" + "</br>" +
+            "Компания развивает собственную сеть авторизованных сервисных центров и пунктов проката электротранспорта, а также образовательных пространств во флагманских магазинах сети.\n" +
+            "</br>" + "</br>",
+        "en": "uStore is a well-visible and recognizable retail on the Ukrainian market, which includes a network of more than 66 stores and an online store uStore.ua.\n" +
+            "We work in a unique for Ukraine format of innovative entertainment centers in a bright modern design, with an area of ​​up to 2000 m².\n" +
+            "These are open access to goods, interactive virtual reality zones, electric vehicles and smart home devices, friendly and knowledgeable staff. A place where you are always welcome.\n" +
+            "</br>" + "</br>" +
+            "The target audience of uStore is active, advanced users of devices and gadgets: from headphones to laptops, from quadcopters to electric bikes.\n" +
+            "We are purposefully engaged in the popularization of electric transport in Ukraine, the first to single out personal transport in a separate category and introduce Ukrainians to this method of transportation.\n" +
+            "In 2016, with the support of uStore, the first unicycle championship of Ukraine was held.\n" +
+            "</br>" + "</br>" +
+            "The online store uStore.ua is confidently among the leaders of online trade in Ukraine.\n" +
+            "The central office is located in Odessa (a virtual tour is available).\n" +
+            "</br>" + "</br>" +
+            "The company develops its own network of authorized service centers and electric vehicle rental points, as well as educational spaces in the flagship stores of the chain.",
+        "ua": "«uStore» - це добре помітний і відомий ритейл на ринку України, що включає мережу з понад 66 магазинів та інтернет-магазин uStore.ua.\n" +
+            "Ми працюємо в унікальному для України форматі інноваційних розважальних центрів у яскравому сучасному дизайні площею до 2000 м².\n" +
+            "Це відкритий доступ до товарів, інтерактивні зони віртуальної реальності, електротранспорт та влаштування розумного будинку, доброзичливий та грамотний персонал. Місце, де вам завжди раді.\n" +
+            "</br>" + "</br>" +
+            "Цільова аудиторія uStoreа – активні, просунуті користувачі девайсів та гаджетів: від навушників до ноутбуків, від квадрокоптерів до електробайків.\n" +
+            "Ми цілеспрямовано займаємося популяризацією електротранспорту в Україні, перші виділили персональний в окрему категорію та познайомили українців із цим способом пересування.\n" +
+            "У 2016 році за підтримки uStoreа було проведено перший чемпіонат України з моноколіс.\n" +
+            "</br>" + "</br>" +
+            "Онлайн магазин uStore.ua упевнено тримається серед лідерів інтернет-торгівлі в Україні.\n" +
+            "Центральний офіс знаходиться в Одесі (доступна віртуальна екскурсія).\n" +
+            "</br>" + "</br>" +
+            "Компанія розвиває власну мережу авторизованих сервісних центрів та пунктів прокату електротранспорту, а також освітніх просторів у флагманських магазинах мережі.",
+    },
+    "aboutSectionP2": {
+        "ru": "История",
+        "en": "History",
+        "ua": "Історія",
+    },
+    "aboutSectionText2": {
+        "ru": "Мы начинали с оптовых продаж мобильных телефонов в 2000 году. Все помнят большие Нокиа и маленькие Эриксон?)\n" +
+            "Первый розничный магазин был открыт в Одессе в июне 2000 года с ярким названием Zitrus.\n" +
+            "</br>" + "</br>" +
+            "Потом был uStore и в конечном итоге мы остановились на хорошо известном сегодня uStore.\n" +
+            "</br>" + "</br>" +
+            "В 2007 интернет-магазин uStore начинает осуществлять доставку в любую точку Украины. На то время – это был прорыв.\n" +
+            "</br>" + "</br>" +
+            "В 2011 открывается первая точка выдачи интернет-магазина.\n" +
+            "uStore делает акцент на технику Apple и расширяет сегмент целевой аудитории.\n" +
+            "</br>" + "</br>" +
+            "История умалчивает в каком именно году мы открыли новый формат магазинов.\n" +
+            "Мы думаем что это было в 2014 или 11? Может кто помнит?)\n" +
+            "</br>" + "</br>" +
+            "Впервые в Украине покупатель увидел открытую выкладку гаджетов и интерактивные демо-зоны вместо застекленных витрин, именно в uStoreе.\n" +
+            "С тех пор телефоны, планшеты и ноутбуки можно трогать, проверять и тестировать. Позже этот прием переняли и другие сети.\n" +
+            "</br>" + "</br>" +
+            "Продавцы-консультанты поменяли строгие костюмы на кеды, футболки и джинсы. В наших магазинах уже тогда была необычная, яркая атмосфера, которая очень к лицу той новизне, которую мы несем.\n" +
+            "</br>" + "</br>" +
+            "В 2014 году компания объявляет старт продаж Xiaomi и Meizu на правах эксклюзивного дистрибьютора и первой знакомит украинцев с китайскими брендами. Это был тяжелый путь, но мы его прошли вместе с вами.\n" +
+            "Купить что-то произведенное в Китае тогда считалось неприемлемым по причине сформированных предубеждений о низком качестве. Годы прошли и теперь негативные убеждения к этим брендам не относятся. Позже продавать продукцию Сяоми и Мейзу стали и другие сети.\n" +
+            "</br>" + "</br>" +
+            "2016 – открыты первые точки uStoreПроката в Одессе и Киеве, в прогулочной зоне Аркадии и на ВДНХ. Отличный способ познакомиться с новым и необычным развлечением на то время и развить новую культуру использования.",
+        "en": "We started as a wholesaler of mobile phones in 2000. Everyone remembers big Nokia and little Erickson?)\n" +
+            "The first retail store was opened in Odessa in June 2000 with the bright name Goolshtaine.\n" +
+            "</br>" + "</br>" +
+            "Then there was uStore and in the end we settled on the well-known uStore today.\n" +
+            "</br>" + "</br>" +
+            "In 2007, the uStore online store starts delivering anywhere in Ukraine. At that time, it was a breakthrough.\n" +
+            "</br>" + "</br>" +
+            "In 2011, the first point of issue of the online store opens.\n" +
+            "uStore focuses on Apple technology and expands the target audience segment.\n" +
+            "</br>" + "</br>" +
+            "History is silent in which year we opened the new store format.\n" +
+            "Do we think it was 2014 or 11? Maybe someone remembers?)\n" +
+            "</br>" + "</br>" +
+            "For the first time in Ukraine, a buyer saw an open display of gadgets and interactive demo zones instead of glazed windows, in uStore.\n" +
+            "Since then, phones, tablets and laptops can be touched, checked and tested. Later, this technique was adopted by other networks.\n" +
+            "</br>" + "</br>" +
+            "Sales assistants swapped formal suits for sneakers, T-shirts and jeans. Even then, there was an unusual, bright atmosphere in our stores, which is very suitable for the novelty that we bring.\n" +
+            "</br>" + "</br>" +
+            "In 2014, the company announces the start of sales of Xiaomi and Meizu as an exclusive distributor and is the first to acquaint Ukrainians with Chinese brands. It was a difficult path, but we went along with you.\n" +
+            "Buying something made in China was then considered unacceptable due to the formed prejudices about low quality. Years have passed and now negative beliefs do not apply to these brands. Later, other networks began to sell products to Xiaomi and Meise.\n" +
+            "</br>" + "</br>" +
+            "2016 - the first points of uStoreProkat were opened in Odessa and Kiev, in the walking area of ​​Arcadia and at VDNKh. A great way to experience new and different entertainment at the time and develop a new culture of use.",
+        "ua": "Ми починали з оптового продажу мобільних телефонів у 2000 році. Усі пам'ятають великі Нокіа та маленькі Еріксон?)\n" +
+            "Перший роздрібний магазин було відкрито в Одесі у червні 2000 року з яскравою назвою Zitrus.\n" +
+            "</br>" + "</br>" +
+            "Потім був uStore та зрештою ми зупинилися на добре відомому сьогодні uStore.\n" +
+            "</br>" + "</br>" +
+            "2007 року інтернет-магазин uStore починає здійснювати доставку в будь-яку точку України. На той час це був прорив.\n" +
+            "</br>" + "</br>" +
+            "У 2011 році відкривається перша точка видачі інтернет-магазину.\n" +
+            "uStore наголошує на техніку Apple і розширює сегмент цільової аудиторії.\n" +
+            "</br>" + "</br>" +
+            "Історія замовчує, в якому саме році ми відкрили новий формат магазинів.\n" +
+            "Ми думаємо що це було у 2014 чи 11? Може, хто пам'ятає?)\n" +
+            "</br>" + "</br>" +
+            "Вперше в Україні покупець побачив відкриту викладку гаджетів та інтерактивні демо-зони замість засклених вітрин саме в uStoreі.\n" +
+            "З того часу телефони, планшети та ноутбуки можна чіпати, перевіряти та тестувати. Пізніше цей прийом перейняли інші мережі.\n" +
+            "</br>" + "</br>" +
+            "Продавці-консультанти змінили суворі костюми на кеди, футболки та джинси. У наших магазинах уже тоді була незвичайна, яскрава атмосфера, яка дуже пасує тій новизні, яку ми несемо.\n" +
+            "</br>" + "</br>" +
+            "У 2014 році компанія оголошує старт продажів Xiaomi та Meizu на правах ексклюзивного дистриб'ютора та першою знайомить українців з китайськими брендами. Це була важка дорога, але ми її пройшли разом з вами.\n" +
+            "Купити щось зроблене в Китаї тоді вважалося неприйнятним через сформовані упередження про низьку якість. Роки минули і тепер негативні переконання до цих брендів не належать. Пізніше продавати продукцію Сяомі та Мейзу стали й інші мережі.\n" +
+            "</br>" + "</br>" +
+            "2016 – відкрито перші точки uStore Прокату в Одесі та Києві, у прогулянковій зоні Аркадії та на ВДНГ. Відмінний спосіб познайомитися з новою та незвичайною розвагою на той час і розвинути нову культуру використання.",
+    },
 }
 
 
